54f461702917a6a0edad9aa6a010514f4a9108ad
Viody Alfaridzi <viody75@gmail.com>
2025-09-27 17:34:30 +0700

    feat: add new recursive functions and examples; comment out test cases for clarity

---
+++ 75_learn_python/lesson_oop.py
@@ -0,0 +1,12 @@
+# lesson 1
+# Summary for lesson 1:
+# - Immutable types (like int, float, str, tuple) are passed by value.
+# - To modify them, you must return the new value and reassign it.
+# - Mutable types (like list, dict, set) can be modified in place without returning
+# Initialize a variable (immutable type: int)
+someVal = 0
+# Function that modifies a parameter but doesn't return it
+# -> menunjukkan bahwa integer (immutable) tidak berubah di luar fungsi
+def incByVal(initVal: int, val: int):
+    initVal += val  # hanya mengubah copy lokal
+    print(f"[incByVal] inside function: {initVal}") 
--- 75_learn_python/lesson_recursive.py
+++ 75_learn_python/lesson_recursive.py
@@ -7,7 +7,7 @@ def count_down(n: int):
     count_down(n-1)
     print(f'Count C : ' + n*'*')
 
-count_down(6)
+# count_down(6)
 
 struktur_folder = {
     "tipe": "folder",
@@ -58,3 +58,43 @@ def count_folder(folder):
 #     for item in folder["isi"]:
 #         total += count_folder(item)
 #     return total
+
+# list_kata = ["Hello", "World", "From", "Python"]
+# print(list_kata[1:])
+# print(list_kata[:1])
+
+def question_1(words: str):
+    if len(words) == 0:
+        return
+    tobe_checked = words[0]
+    if tobe_checked.isdigit():
+        print(f"{tobe_checked} adalah angka")
+        return
+    question_1(words[1:])
+
+# question_1('Saya ingin menjabat 3 periode')
+
+def recursive_sqrt(n: int):
+    assert n > 0, "Tidak boleh kurang dari sama dengan 0"
+    # if n < 2:
+    #     return n
+    # return recursive_sqrt(n // 2)
+    
+# print(recursive_sqrt(16))  # Output: 4
+
+list_bilangan = [1, 2, 3, 4, 5]
+
+def question_3(list : list, x : int ):
+    
+    if len(list) == 0:
+        return 0
+    new_list = list[1:]
+    if list[0] == x:
+        print('Yes')
+        return question_3(new_list, x )
+    else:
+        print('No')
+        return list[0] + question_3(new_list, x )
+
+# question_3(list_bilangan, 15)
+# print(question_3(list_bilangan, 16))
